0ff27b4f5218019b5ff463e3defe1d20dad448f5
shreyapattewar123 <shreyapattewar14@gmail.com>
2024-10-16 06:41:05 +0000

    add

--- Frontend/src/component/home/HomePage.jsx
+++ Frontend/src/component/home/HomePage.jsx
@@ -23,7 +23,7 @@ const HomePage = () => {
                     <div className="overlay"></div>
                     <div className="animated-texts overlay-content">
                         <h1>
-                            Welcome to <span className="phegon-color">Taj Hotel</span>
+                            Welcome to <span className="taj-color">Taj Hotel</span>
                         </h1><br />
                         <h3>Step into a haven of comfort and care</h3>
                     </div>
